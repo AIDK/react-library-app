90b383ce0d1807a22467f91e2c107d2c22881420
AndriesDK <andriesd@cashcrusaders.co.za>
2025-02-25 11:57:15 +0200

    added project images and icons, styled main layout page header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+
+const Home = () => {
+  return (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  );
+};
+
+export default Home;
