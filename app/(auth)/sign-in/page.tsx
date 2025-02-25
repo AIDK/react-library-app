33900660e1e73bf096625cabcae4cf2b25708ae9
AndriesDK <andriesd@cashcrusaders.co.za>
2025-02-25 18:38:01 +0200

    added sign in and sign up form

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,18 @@
+﻿"use client";
+
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/Validations";
+
+const Page = () => (
+  <AuthForm
+    type={"SIGN_IN"}
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+    }}
+    onSubmit={() => {}}
+  />
+);
+export default Page;
