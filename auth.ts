5b6019564fbe1e760bd006b1cd06133aaed204d4
Andries de Kock <53447003+AIDK@users.noreply.github.com>
2025-02-26 14:34:16 +0200

    User creation (#4)
    * implemented Auth.js
    
    * fixed login authentication issue, implemented loading user initials when logged in
    
    * added logout from profile page
    
    ---------
    
    Co-authored-by: AndriesDK <andriesd@cashcrusaders.co.za>

---
+++ auth.ts
@@ -0,0 +1,71 @@
+﻿import NextAuth, { User } from "next-auth";
+import { compare } from "bcryptjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        // get user where email matches db record and limiting to 1
+        // (there should only ever be 1 result as email is unique)
+        const user = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.email.toString()))
+          .limit(1);
+
+        if (user.length === 0) return null;
+
+        // compare whether user's password matches the encrypted password
+        const isPasswordValid = await compare(
+          credentials.password.toString().trim(),
+          user[0].password.trim(),
+        );
+
+        if (!isPasswordValid) return null;
+
+        console.log(
+          "returned user",
+          user[0].id.toString(),
+          user[0].fullName,
+          user[0].email,
+        );
+
+        return {
+          id: user[0].id.toString(),
+          email: user[0].email,
+          name: user[0].fullName,
+        } as User; // this user comes from NextAuth (it is not our declared user structure)
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+});
